6ecfd096eaef834e3eec374694c4a3be0c403ce3
Cosmic <aviralsingh@protonmail.com>
2020-05-31 16:58:48 +0530

    Add basic functionality

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-#Make a class BullCowGame
+from settings import Settings
+
+
+# Make a class BullCowGame
+class BullCowGame:
+
     # Bring in word and other setting
+    def __init__(self):
+        self.settings = Settings()
+        self.turns = self.settings.turns
+        self.word_length = self.settings.word_length
+        self.word = self.settings.word
 
-    # Print intro
+    # Print introduction
+    def intro(self):
+        print('Hello! Welcome to the Bull Cow Game!')
 
-    # Print the question
+    # Ask the question and record the input
+    def question(self):
+        self.input = input('Guess the ' + str(self.word_length)
+                           + ' letter isogram: ')
 
-    # Record the input
+    # Match the input with the answer
 
-    # Match the input with the answer and display the bulls and cows
+    # Display Bulls and Cows
 
-    # Increment the number of turns 
+    # Increment the number of turns
 
     #  Display whether the player won or lost
 
     # Main
+    def main(self):
+        self.intro()
+        self.question()
+
+game = BullCowGame()
+game.main()
