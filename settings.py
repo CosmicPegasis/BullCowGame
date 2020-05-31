0570918c23cde0e9eb0a9ec94959aa447b40be12
Cosmic <aviralsingh@protonmail.com>
2020-05-31 16:58:20 +0530

    Add basic data

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Settings class
+# Contains info like turns, the word etc.
+
+class Settings:
+    def __init__(self):
+        self.turns = 7
+        self.word_length = 5
+        self.word = 'badge' 
+    
