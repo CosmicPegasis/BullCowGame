8668a8e81dc730fe1f9540075b7ebfdc1205f494
Cosmic <aviralsingh@protonmail.com>
2020-05-31 16:13:27 +0530

    Initial Commit With Psuedo Code

---
+++ main.py
@@ -0,0 +1,16 @@
+#Make a class BullCowGame
+    # Bring in word and other setting
+
+    # Print intro
+
+    # Print the question
+
+    # Record the input
+
+    # Match the input with the answer and display the bulls and cows
+
+    # Increment the number of turns 
+
+    #  Display whether the player won or lost
+
+    # Main
